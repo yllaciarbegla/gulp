9f55eb05c68081206ec7b5f1d0fab986703ea1b7
yllaciarbegla <88842440+yllaciarbegla@users.noreply.github.com>
2022-11-10 00:55:52 +0200

    added files

---
+++ gulp/tasks/js.js
@@ -0,0 +1,12 @@
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+
+export const js = () => {
+	return app.gulp.src(app.path.src.js, {sourcemaps: true})
+		.pipe(uglify())
+		.pipe(rename({
+			extname: '.min.js'
+		}))
+		.pipe(app.gulp.dest(app.path.build.js))
+		.pipe(app.plugins.browsersync.stream());
+}
