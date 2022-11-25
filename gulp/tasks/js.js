e7f80d4415432ca652a90b4370a8495233bfa16a
yllaciarbegla <yllaciarbegla@gmail.com>
2022-11-25 14:26:39 +0200

    js to head; unmin js to dist

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,7 +2,8 @@ import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 
 export const js = () => {
-	return app.gulp.src(app.path.src.js, {sourcemaps: true})
+	return app.gulp.src(app.path.src.js, { sourcemaps: true })
+		.pipe(app.gulp.dest(app.path.build.js))
 		.pipe(uglify())
 		.pipe(rename({
 			extname: '.min.js'
