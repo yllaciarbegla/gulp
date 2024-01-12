0e7489a5729814f2c412a90531139a94812fab3f
yllaciarbegla <yllaciarbegla@gmail.com>
2024-01-13 00:17:43 +0200

    SCSS @img-alias handler
    SCSS: '@img' alias handler after compilation (works correctly in subfolders)

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -9,11 +9,11 @@ import groupCssMediaQueries from 'gulp-group-css-media-queries';
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-	return app.gulp.src(app.path.src.scss, {sourcemaps: true})
-		.pipe(app.plugins.replace(/@img\//g, '../img/'))
+	return app.gulp.src(app.path.src.scss, { sourcemaps: true })
 		.pipe(sass({
 			outputStyle: 'expanded'
 		}))
+		.pipe(app.plugins.replace(/@img\//g, '../img/'))
 		.pipe(groupCssMediaQueries())
 		.pipe(autoprefixer({
 			overrideBrowserslist: ['last 3 versions']
